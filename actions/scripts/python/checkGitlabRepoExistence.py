805649a53915942b8ad2999c6a5cc32ce702eb83
SKottarathi <skottarathi@in.imshealth.com>
2017-05-17 14:17:52 +0530

    Added action for checing if a gitlab repo exist or not DEVOPS-217

---
+++ actions/scripts/python/checkGitlabRepoExistence.py
@@ -0,0 +1,25 @@
+from st2actions.runners.pythonrunner import Action
+import gitlab
+
+class checkGitlabRepository(Action):
+    def run(self,projectName):
+    #Will create a Gitlab repository .Add a README.MD file with default contents and will create Master and Developmnt branches
+        gitLabUrl=self.config["gitLabUrl"]
+        gitLabUserName=self.config["gitLabUsername"]
+        gitLabPrivateToken=self.config["gitLabPrivateToken"]
+        connectGitLab = gitlab.Gitlab(gitLabUrl,gitLabPrivateToken,ssl_verify=False)
+        self.logger.info('Connecting to GitLab')
+        connectGitLab.auth()
+        self.logger.info(connectGitLab.auth())
+        getAllGitLabProjects=connectGitLab.projects.list()
+        getAllGitLabProjectNames=[]
+        for gitLabProject in getAllGitLabProjects:
+            getAllGitLabProjectNames.append(gitLabProject.name)
+            if projectName in getAllGitLabProjectNames:
+                self.logger.error("Repo exists")
+                return {"RepositoryExistence": "yes"}
+            else:
+                return {"RepositoryExistence": "no"}
+
+
+
