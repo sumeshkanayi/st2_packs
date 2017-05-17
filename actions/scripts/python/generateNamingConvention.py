460889fd7498f11d3869f6494568a87312baf43f
SKottarathi <skottarathi@in.imshealth.com>
2017-05-17 15:32:50 +0530

    Added action for  generating naming convention  for the gitlab repository .Fixing python intending .DEVOPS-217

--- actions/scripts/python/generateNamingConvention.py
+++ actions/scripts/python/generateNamingConvention.py
@@ -3,15 +3,15 @@ from st2actions.runners.pythonrunner import Action
 class generateNamingConvention(Action):
  def run(self,projectCategory,description,API,componentName,projectFacing):
     self.logger.info("GitLab repository creation completed")
-        if projectFacing == "internal" :
+    if projectFacing == "internal" :
             projectFqdn="com.rxcorp."
-        else:
+    else:
             projectFqdn="com.quintilesims."
-        if API is None:
+    if API is None:
             projectName=(projectFqdn+projectCategory+'.'+componentName).lower()
-        else:
+    else:
          projectName=(projectFqdn+projectCategory+'.'+componentName+'.'+API).lower()
-        return {"projectName": projectName,"Category": projectCategory}
+    return {"projectName": projectName,"Category": projectCategory}
 
 
 
