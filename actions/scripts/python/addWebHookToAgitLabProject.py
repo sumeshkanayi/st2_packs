a0c818db69e655f5289cc78353f9d7bf545208c7
sumeshkanayi <sumeshkanayi@gail.com>
2017-05-18 09:34:36 +0530

    Added webhook feature

---
+++ actions/scripts/python/addWebHookToAgitLabProject.py
@@ -0,0 +1,17 @@
+from st2actions.runners.pythonrunner import Action
+import gitlab
+
+class createGitlabRepository(Action):
+    def run(self,projectId,webHookUrl):
+        gitLabUrl=self.config["gitLabUrl"]
+        gitLabUserName=self.config["gitLabUsername"]
+        gitLabPrivateToken=self.config["gitLabPrivateToken"]
+        connectGitLab = gitlab.Gitlab(gitLabUrl,gitLabPrivateToken,ssl_verify=False)
+        self.logger.info('Connecting to GitLab')
+        connectGitLab.auth()
+        self.logger.info(connectGitLab.auth())
+        gitLabwebHookCreationStatus=connectGitLab.projects_hooks.create({'url': webHookUrl,'push_events': 1},project_id=projectId)
+
+
+
+
